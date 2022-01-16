097a44775c91d37082bb188bf3c1280ff2c0bf94
Alex Krueger <alexckrueger@gmail.com>
2022-01-15 21:18:36 -0600

    adds post correctly this time

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   get "/message" => "params#message"
   get "/:message" => "params#message"
   post "/message" => "params#message"
-  post "/:message" => "params#message"
 end
